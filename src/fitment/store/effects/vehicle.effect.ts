4c7eb27f4f919c7ceefc23264c0e06edfe8a2ed9
padma32901 <padma32901@gmail.com>
2021-04-28 20:53:50 +0530

    1 file modified

--- src/fitment/store/effects/vehicle.effect.ts
+++ src/fitment/store/effects/vehicle.effect.ts
@@ -2,7 +2,7 @@ import { Injectable } from "@angular/core";
 import { Actions, Effect, ofType } from "@ngrx/effects";
 import { of } from "rxjs";
 
-import { map, switchMap, catchError } from "rxjs/operators";
+import { map, switchMap, catchError, mergeMap } from "rxjs/operators";
 import { VehiclesService } from "../../../app/services/vehicles.service";
 
 import * as vehicleAction from "../actions";
@@ -18,7 +18,7 @@ export class VehicleEffect {
 
   @Effect()
   getYears = this._actions.pipe(
-    switchMap(action => {
+    mergeMap(action => {
       return this._vehiclesService.getYears().pipe(
         map((response: any) => new LoadYearsSuccess({ years: response.year })),
         catchError(error => of(new LoadYearsFail(error)))
